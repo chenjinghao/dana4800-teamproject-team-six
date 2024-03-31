ce773059b40ae517892f965685fd18239324788b
Adam CHEN JINGHAO <jinghao_chen@hotmail.com>
2024-03-31 12:08:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,9 @@ import streamlit as st
 
 st.title('DANA 4800 Team Project Phase 2')
 
-team_members = '''<ul>\
-<li>Ananta Arora</li>\
-  <li>Jinghao Chen</li>\
-  <li>Roxanne Alvarez</li>\
-  <li>Teshani Jayasinghe</li>\
-</ul>'''
+team_members = '''<ul>
+<li>Yash Shah</li>
+<li>Shivam Patel</li>'''
 
 # Using "with" notation
 with st.sidebar:
