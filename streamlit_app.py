ab0e741efcdc746981ac4e0b5c8176eb14408c6e
Adam CHEN JINGHAO <jinghao_chen@hotmail.com>
2024-03-31 11:52:26 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-%matplotlib inline
 
 from scipy.stats import chi2_contingency
 from scipy import stats
