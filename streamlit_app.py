6aec1eb46cf57a7b48fe64b1166ffb93ad344cea
Adam CHEN JINGHAO <jinghao_chen@hotmail.com>
2024-03-31 14:57:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ with st.sidebar:
     st.subheader('Between Models')
     num_features_option = st.selectbox(
         "Number of features", 
-        ("67","29"),)
+        ("67","29"),placeholder=True)
     st.write(f'You selected: {num_features_option} features.' )
     st.header("SHAP Analysis")
     st.header("LIME Analysis")
