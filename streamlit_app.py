cce7d8e0d772a19644bb15fbcb7633fd593f3485
Adam CHEN <jinghao_chen@hotmail.com>
2024-03-31 11:04:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,6 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# DANA 4800 Team Project Phase 2
+## Team 6
 """
