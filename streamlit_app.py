4feac6ee678abc82393873da662954a5845d8d17
Adam CHEN <jinghao_chen@hotmail.com>
2024-03-31 11:14:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,16 @@ import streamlit as st
 # DANA 4800 Team Project Phase 2
 ## Team 6
 """
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
