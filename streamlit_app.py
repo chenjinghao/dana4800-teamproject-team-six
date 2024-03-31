6f4010b0b1884e2f56dd9e0c6e5d9287af283b8e
Adam CHEN JINGHAO <jinghao_chen@hotmail.com>
2024-03-31 12:07:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ import streamlit as st
 
 st.title('DANA 4800 Team Project Phase 2')
 
-team_members = '''<ul>
-  <li>Ananta Arora</li>
-  <li>Jinghao Chen</li>
-  <li>Roxanne Alvarez</li>
-  <li>Teshani Jayasinghe</li>
+team_members = '''<ul>\
+<li>Ananta Arora</li>\
+  <li>Jinghao Chen</li>\
+  <li>Roxanne Alvarez</li>\
+  <li>Teshani Jayasinghe</li>\
 </ul>'''
 
 # Using "with" notation
