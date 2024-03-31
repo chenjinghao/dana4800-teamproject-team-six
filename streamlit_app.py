ca2bf2ce54b689f12f41e9cbcb1057b56e62f737
Adam CHEN JINGHAO <jinghao_chen@hotmail.com>
2024-03-31 11:39:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,12 @@ import streamlit as st
 # Using "with" notation
 with st.sidebar:
     st.title("Section")
+    st.header("Performance Comparision")
+    st.header("Team Members")
+    st.markdown("- Ananta Arora (SID: 100421624)\
+                - Jinghao Chen (SID: 100406201)\
+                - Roxanne Alvarez (SID: 100405742)\
+                - Teshani Jayasinghe (SID: 100422405)")
 
 
 
