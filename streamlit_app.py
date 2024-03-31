4d6664486d58adf39c29dc5812c2312a015d3030
Adam CHEN JINGHAO <jinghao_chen@hotmail.com>
2024-03-31 13:15:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import altair as alt
 import streamlit as st
+import pandas as pd
 
-st.title('DANA 4800 Team Project Phase 2')
+st.title('DANA 4800 Team Project Phase 2',
+         divider='rainbow')
+st.subheader('Interactive Dashboard:computer:')
+
 
 team_members = '''
 - **Ananta Arora**
@@ -9,10 +13,15 @@ team_members = '''
 - **Roxanne Alvarez**
 - **Teshani Jayasinghe**'''
 
+
+
 # Using "with" notation
 with st.sidebar:
     st.title("Section")
     st.header("Performance Comparision")
+    st.subheader('Between Models')
+    num_features_option = st.selecbox('Select the number of features', [67,29])
+    st.write('You selected:', num_features_option)
     st.header("SHAP Analysis")
     st.header("LIME Analysis")
     st.header("Team Members")
